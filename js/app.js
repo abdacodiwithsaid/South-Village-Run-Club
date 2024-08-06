56ceb2be0577ae3b0fb3d274517fa67d4f7d324a
Said BELAIB <abdacodi@gmail.com>
2024-08-06 19:09:41 +0100

    Add Responsive Navigation Bar

--- js/app.js
+++ js/app.js
@@ -3,9 +3,7 @@ const navMenu = document.querySelector(".navbar-links");
 
 hamburger.addEventListener("click", mobileMenu);
 
-/*
- The `mobileMenu` function toggles the "active" class on both the hamburger element and the navigation menu element.
- */
+// The `mobileMenu` function toggles the "active" class on both the hamburger element and the navigation menu element.
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -15,9 +13,7 @@ const navLink = document.querySelectorAll(".navbar-links li");
 
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-/**
- * The function `closeMenu` removes the "active" class from both the hamburger and navMenu elements.
- */
+// The function `closeMenu` removes the "active" class from both the hamburger and navMenu elements.
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
